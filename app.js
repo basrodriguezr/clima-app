2a963965fd5981d907fb6c26ad92fba9d3625a31
bastian rodríguez <basrodriguezr@gmail.com>
2022-06-06 13:22:38 -0400

    preparando el modelo

--- app.js
+++ app.js
@@ -1,20 +1,37 @@
-const { inquireMenu, pausa} = require("./helpers/inquirer");
+const { inquireMenu, pausa, leerInput} = require("./helpers/inquirer");
+const Busquedas = require("./models/busquedas");
 
 const main = async() => {
     let opt;
+    const busquedas = new Busquedas();
 
     while(opt!= 0){
         opt =  await inquireMenu();
         
         switch (opt) {
             case 1:
-            
+                //mostrar mensaje
+                const lugar = await leerInput('Lugar: ');
+                console.log(lugar);
+                //buscar lugares
+                //seleccionar lugar
+                //clima
+                //mostrar resultados
+                console.log('\ninformación de la ciudad\n'.green);
+                console.log('Ciudad: ',);
+                console.log('Lat: ',);
+                console.log('Lon: ',);
+                console.log('Temperatura: ',);
+                console.log('Mínima: ',);
+                console.log('Máxima: ',);
+
             break;
             
             case 2:
                 
             break;     
-        }  
+        } 
+
         await pausa(); 
     }
 
---
+++ models/busquedas.js
@@ -0,0 +1,16 @@
+class Busquedas{
+    historial = [];
+
+    constructor(){
+        // leer db si existe
+    }
+
+    async ciudad(lugar = ''){
+        //peticion http
+
+        console.log(lugar);
+        return []; //retorna lugares
+    }
+}
+
+module.exports = Busquedas;
