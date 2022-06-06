a1d322435783c24f70848dcdcc061570a274a9ad
bastian rodríguez <basrodriguezr@gmail.com>
2022-06-06 14:15:22 -0400

    mapbox search api y token de acceso

--- models/busquedas.js
+++ models/busquedas.js
@@ -8,11 +8,16 @@ class Busquedas{
     }
 
     async ciudad(lugar = ''){
-        //peticion http
-        //console.log('ciudad ', lugar);
-        const resp= await axios.get('https://reqres.in/api/users?page=2')
-        console.log(resp.data);
-        return []; //retorna lugares
+        try{
+            //peticion http
+            //console.log('ciudad ', lugar);
+            const resp= await axios.get('https://api.mapbox.com/geocoding/v5/mapbox.places/%C3%B1u%C3%B1oa.json?proximity=-73.990593%2C40.740121&language=es&access_token=pk.eyJ1IjoiYmVpc3MiLCJhIjoiY2w0MzFjY3MxMGJqMDNibjI5OGk4cHdlbiJ9.fE1z0G7TLY5mUDICuEOpvA')
+            console.log(resp.data);
+            return []; //retorna lugares
+        }catch(error){
+            return [];  
+        }
+       
     }
 }
 
